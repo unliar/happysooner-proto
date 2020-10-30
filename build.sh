8413747119ea175e97473b4829d1679ae784353d
blessyou <370732889@qq.com>
2020-10-30 14:03:49 +0800

    feat: add github ci time

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ find . -name *.proto |
 while read filename
 do
     echo "proto-compiler $filename running...."
-    protoc --micro_out=. --go_out=. $filename    
+    protoc --micro_out=. --go_out=. $filename
+    echo "proto-compiler $filename done"
 done
